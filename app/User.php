d430833e4a920c575d11d5ce17772a72f7d14a7f
Hein Zin <heinzin528@gmail.com>
2015-09-26 16:35:47 +0630

    Update User.php

--- app/User.php
+++ app/User.php
@@ -5,20 +5,23 @@ namespace App;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+class User extends Model implements AuthenticatableContract, 
+                                    AuthorizableContract,
+                                    CanResetPasswordContract
 {
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, Authorizable, CanResetPassword;
 
     /**
      * The database table used by the model.
      *
      * @var string
      */
-    protected $table = 'users';
+    // protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -32,7 +35,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    // protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['password'];
 
 
     public function checkLikeCandidate($candidateId)
