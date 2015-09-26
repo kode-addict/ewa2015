7e5f307e257deb5ae533a2dafe937c7d5ba4e26a
Hein Zin <heinzin528@gmail.com>
2015-09-26 16:18:02 +0630

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -54,17 +54,13 @@ Route::get('login', function ()
 {
     return redirect('auth/login');
 });
-
 Route::get('auth/login', 'Auth\AuthController@getLogin');
-
 Route::post('auth/login', 'Auth\AuthController@postLogin');
-
 Route::get('logout', function ()
 {
     return redirect('auth/logout');
 });
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
 // Registration routes...
 Route::get('register', function ()
 {
@@ -72,14 +68,10 @@ Route::get('register', function ()
 });
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-
 Route::controllers([
    'password' => 'Auth\PasswordController',
 ]);
-
 // AuthenticatedUser area
-
 $router->group([
         'namespace' => 'Auth',
         'middleware' => 'auth',
@@ -88,18 +80,12 @@ $router->group([
       	return "Welcome MaePaySoh";
       });
     });
-
 // FbUser area
-
 Route::get('auth/login_with_fb', 'Auth\AuthController@redirectToProvider');
-
 Route::get('auth/callback', 'Auth\AuthController@handleProviderCallback');
-
-
 // Password reset link request routes...
 Route::get('password/email', 'Auth\PasswordController@getEmail');
 Route::post('password/email', 'Auth\PasswordController@postEmail');
-
 // Password reset routes... 
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
